dceb149de389c13a33cba69d919f8f1f4461f409
Mike MacCana <mike.maccana@gmail.com>
2023-03-24 22:59:59 +0000

    use async/await in message handling

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,18 @@
-const VERSION = 26;
-console.log(`hello from service worker ${VERSION}`);
-chrome.runtime.onMessage.addListener(async ({ name, options }) => {
-  console.log(
-    `🍈 Service worker got a message from elsewhere in the extension`
-  );
+console.log(`hello from service worker.`);
+
+const SECOND = 1000;
+
+// Just a demo async function
+export const sleep = async (timeInMs) => {
+  return new Promise((resolve) => setTimeout(resolve, timeInMs));
+};
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  (async () => {
+    await sleep(1 * SECOND);
+    console.log(
+      `🍈 Service worker got a message from elsewhere in the extension`
+    );
+  })();
+  return true;
 });
